47b1a99310eac7a256f64bd5c41c7aa36259f8d6
MultiBug <multibug90@gmail.com>
2021-05-20 15:15:02 +0300

    Update sololearn.sql

--- sololearn.sql
+++ sololearn.sql
@@ -5,3 +5,13 @@ CREATE TABLE Persons (
     Address varchar(255),
     City varchar(255)
 );
+
+SELECT PersonID, LastName, FirstName, Address, City
+FROM Persons;
+
+
+
+INSERT INTO Persons
+(PersonID, LastName, FirstName, Address, City)
+VALUES(0, 'Alan', 'Don', 'Stive', 'Ben');
+
